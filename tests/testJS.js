717aea31b6f66b42831643294410ef604d29ae2a
DevonMM <Devon.m.moran@gmail.com>
2019-03-02 23:21:27 -0500

    stillerrors

--- tests/testJS.js
+++ tests/testJS.js
@@ -1,5 +1,7 @@
+var expect = require("karma-chai")
+var game = require("../bowlingGame")
 describe("BowlingGameWhole", function() {
-	var game;
+	
 
 	beforeEach(function(){
 		game = new BowlingGameWhole();
@@ -20,24 +22,24 @@ describe("BowlingGameWhole", function() {
 		game.roll(10);
 	}
 
-	it("keeps track of the gutterball game", function() {
+	it("should keep track of the gutterball game", function() {
 		rollNumber(20, 0);
 		expect(game.score()).to.equal(0);
 	});
 
-	it("handles anytime there is 1 pin that falls", function() {
+	it("should handle anytime there is 1 pin that falls", function() {
 		rollNumber(20, 1);
 		expect(game.score()).to.equal(20);
 	});
 
-	it("handles 1 spare that is rolled", function() {
+	it("should handle 1 spare that is rolled", function() {
 		rollsSpare();
 		game.roll(3);
 		rollNumber(17, 0);
 		expect(game.score()).to.equal(16);
 	});
 
-	it("handles when a strike (all ten pins at once) is rolled", function() {
+	it("should handle when a strike (all ten pins at once) is rolled", function() {
 		rollsStrike();
 		game.roll(3);
 		game.roll(4);
@@ -45,7 +47,7 @@ describe("BowlingGameWhole", function() {
 		expect(game.score()).to.equal(24);
 	});
 
-	it("handles a bowler doing a perfect game", function() {
+	it("should handle a bowler doing a perfect game", function() {
 		rollNumber(12, 10);
 		expect(game.score()).to.equal(300);
 	});
