8d3e5b33459f3e800c3889af00619ca6d80ef41a
DevonMM <Devon.m.moran@gmail.com>
2019-03-04 23:40:11 -0500

    finalchanges

--- tests/testJS.js
+++ tests/testJS.js
@@ -1,59 +1,54 @@
 
 	
 describe("BowlingGameWhole", function() {
-	
-		// var game;
-	
-	// var expect = require("karma-chai")
-	// var game = require("../bowlingGame.js");
-	// function rollNumber (n, pins) {
-	// 	while (n--) {
-	// 		game.roll(pins)
-	// 	}
-	// }
+	var game;
+	this.beforeEach(function(){
+		game = new BowlingGameWhole();
 
-	// function rollsSpare() {
-	// 	game.roll(3);
-	// 	game.roll(7);
-	// }
-
-	// function rollsStrike() {
-	// 	game.roll(10);
-	// }
-
-	it("should keep track of the gutterball game", function() {
-		var game = new BowlingGameWhole();
-		for (var i = 0; i < 20;i++) {
-		// rollNumber(20, 0);
-		game.rollBall(0);
+	function rollNumber (n, pins) {
+		for (var i = 0; i < n; i++) {
+			game.rollBall(pins)
+		}
 	}
+
+	function rollsSpare() {
+		game.rollBall(5);
+		game.rollBall(5);	
+	}
+
+	function rollsStrike() {
+		game.rollBall(10);
+	}
+	
+	it("should keep track of the gutterball game", function() {
+		rollNumber(20,0);
 		expect(game.currentScore()).to.equal(0);
 	});
 
+	it("should give data any time 1 or more pin falls", function() {
+		rollNumber(20, 1);
+		expect(game.currentScore()).to.equal(20);
+	});
 
-// 	it("should handle anytime there is 1 pin that falls", function() {
-// 		rollNumber(20, 1);
-// 		expect(game.score()).to.equal(20);
-// 	});
+	it("should be able to give data for spare", function() {
+		rollsSpare();
+		game.rollBall(3);
+		rollNumber(16, 0);
+		expect(game.currentScore()).to.equal(21);
+	});
 
-// 	it("should handle 1 spare that is rolled", function() {
-// 		rollsSpare();
-// 		game.roll(3);
-// 		rollNumber(17, 0);
-// 		expect(game.score()).to.equal(16);
-// 	});
-
-// 	it("should handle when a strike (all ten pins at once) is rolled", function() {
-// 		rollsStrike();
-// 		game.roll(3);
-// 		game.roll(4);
-// 		rollNumber(16, 0);
-// 		expect(game.score()).to.equal(24);
-// 	});
-
-// 	it("should handle a bowler doing a perfect game", function() {
-// 		rollNumber(12, 10);
-// 		expect(game.score()).to.equal(300);
-// 	});
-// });
+	it("should be able to give data for a strike", function() {
+		rollsStrike();
+		game.rollBall(3);
+		game.rollBall(4);
+		rollNumber(16, 0);
+		expect(game.currentScore()).to.equal(24);
+	});
+	it("should give data for a perfect game", function() {
+		rollNumber(12, 10);
+		expect(game.currentScore()).to.equal(300);
+	})
 });
+});
+
+	
